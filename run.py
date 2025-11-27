ee9ce27aab71cb831612ec9b1dde3f65fec40dbe
luis2999tlvhotmailcom-creator <luis2999tlvhotmail.com@gmail.com>
2025-11-27 04:44:52 +0000

    Arreglando run.py para Render

--- run.py
+++ run.py
@@ -3,12 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # <- importantísimo
+load_dotenv()
 
 from app import create_app, db
 
+# --- ¡ESTA ES LA LÍNEA QUE ARREGLA TODO! ---
+# La sacamos del 'if' para que Render la pueda ver
+app = create_app()
+# -------------------------------------------
+
 if __name__ == "__main__":
-    app = create_app()
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=5000)
